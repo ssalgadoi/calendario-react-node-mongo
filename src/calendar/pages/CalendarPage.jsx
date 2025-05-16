4ebc0e69827823fc47cf276aff00b99d4b9ece26
ssalgadoi <se.salgado@duocuc.cl>
2025-05-16 11:36:04 -0400

    Agrego React Big Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,44 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import enUS from 'date-fns/locale/en-US'
 import { NavBar } from "../components/NavBar"
+import { addHours, parse, startOfWeek, getDay, format } from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Cumpleaños del Jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Chipy'
+  }
+}]
 
 
 export const CalendarPage = () => {
   return (
     <>
-    <NavBar/>
+      <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px)' }}
+      />
     </>
   )
 }
